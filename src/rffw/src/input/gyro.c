4814a1e62538bd901155afb2f4d18f33f6615312
Sean Vig <sean.v.775@gmail.com>
2016-07-22 02:24:18 -0500

    Make it a mouse again, this time as a pointer!

--- src/rffw/src/input/gyro.c
+++ src/rffw/src/input/gyro.c
@@ -2,9 +2,14 @@
 
 #include <stdint.h>
 
+#include "usbd_hid.h"
+#include "usb_device.h"
+
 #include "input/gyro.h"
 
 #define CALIBRATION_CYCLES 1000
+
+static int8_t hidBuffer[4];
 
 static uint16_t calibrationCycles = CALIBRATION_CYCLES;
 
@@ -15,6 +20,12 @@ static void updateCalibration(int16_t *rawGyro)
     static int32_t gyroSum[3];
     uint8_t axis;
 
+    if (calibrationCycles == CALIBRATION_CYCLES) {
+        for (axis = 0; axis < 3; axis++) {
+            gyroSum[axis] = 0;
+        }
+    }
+
     for (axis = 0; axis < 3; axis++) {
         gyroSum[axis] += rawGyro[axis];
     }
@@ -22,7 +33,7 @@ static void updateCalibration(int16_t *rawGyro)
     if (--calibrationCycles == 0) {
         for (axis = 0; axis < 3; axis++) {
             // add what comes out to be 1/2 to improve rounding
-            gyroCalibration[axis] = (gyroCalibration[axis] + (CALIBRATION_CYCLES / 2)) / CALIBRATION_CYCLES;
+            gyroCalibration[axis] = (gyroSum[axis] + (CALIBRATION_CYCLES / 2)) / CALIBRATION_CYCLES;
         }
     }
 }
@@ -39,4 +50,12 @@ void updateGyro(int16_t *rawGyro, float scale)
     for (axis = 0; axis < 3; axis++) {
         rawGyro[axis] -= gyroCalibration[axis];
     }
+
+    // HID stuff
+    hidBuffer[0] = 0;
+    hidBuffer[1] = (int8_t)(rawGyro[0] * scale);
+    hidBuffer[2] = (int8_t)(rawGyro[1] * scale);
+    hidBuffer[3] = 0;
+
+    USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t*)hidBuffer, 4);
 }
