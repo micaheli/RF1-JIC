5ede6b2f180e19168054d98767c0b52382df76dd
Kalyn Doerr <kalyn.doerr@gmail.com>
2016-08-14 16:07:22 -0500

    Getting gyro data over HID

--- src/rffw/src/input/gyro.c
+++ src/rffw/src/input/gyro.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "includes.h"
+
 #include "usbd_hid.h"
 #include "usb_device.h"
 
@@ -12,7 +14,7 @@
 
 #define CALIBRATION_CYCLES 1000
 
-static int8_t hidBuffer[4];
+extern uint8_t tInBuffer[];
 
 static uint32_t calibrationCycles = CALIBRATION_CYCLES * 100;
 
@@ -48,10 +50,24 @@ void updateGyro(int16_t *rawGyro, float scale)
     accgyroDeviceApplyCalibration(rawGyro);
 
     // HID stuff
-    hidBuffer[0] = 0;
-    hidBuffer[1] = (int8_t)(rawGyro[0] * scale);
-    hidBuffer[2] = (int8_t)(rawGyro[1] * scale);
-    hidBuffer[3] = 0;
+    LED1_TOGGLE;
+	tInBuffer[0] = 1;
+	tInBuffer[1]=(int8_t)(rawGyro[0] * scale);;
+	tInBuffer[2]=(int8_t)(rawGyro[1] * scale);;
+	tInBuffer[3]=(int8_t)(rawGyro[2] * scale);;
+	tInBuffer[4]=0x65;
+	tInBuffer[5]=0x46;
+	tInBuffer[6]=0x6c;
+	tInBuffer[7]=0x69;
+	tInBuffer[8]=0x67;
+	tInBuffer[9]=0x68;
+	tInBuffer[10]=0x74;
+	tInBuffer[11]=0x20;
+	tInBuffer[12]=0x46;
+	tInBuffer[13]=0x54;
+	tInBuffer[14]=0x57;
+	tInBuffer[15]=0x21;
+	tInBuffer[16]=0x21;
+    USBD_HID_SendReport (&hUsbDeviceFS, tInBuffer, HID_EPIN_SIZE);
 
-    USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t*)hidBuffer, 4);
 }
