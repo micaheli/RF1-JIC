fbe537dbc421e2d981db913446cc65f10e716e6c
Kalyn Doerr <kalyn.doerr@gmail.com>
2016-09-25 10:04:54 -0700

    catfish

--- src/target/boarddef.c
+++ src/target/boarddef.c
@@ -1,11 +1,12 @@
 #include "includes.h"
 
 int InitializeMcu() {
-	target_pinout pins;
+	//target_pinout pins;
 
 	bzero(pins, sizeof(pins));
+	pins[0]=GPIOA;
 
-	pins.usart_ports[0].rxGPIO = GPIOA;
+	//pins.usart_ports[0].rxGPIO = GPIOA;
 
 	return(1);
 
