5104971067713509a63d2f45632237709805964c
Preston Garrison <Preston Garrison>
2016-09-25 10:23:45 -0700

    more

--- src/target/boarddef.c
+++ src/target/boarddef.c
@@ -1,6 +1,8 @@
 #include "includes.h"
 
-GPIO_TypeDef *pins[256]; // last byte is count
+uint16_t pins[256]; // last byte is count
+GPIO_TypeDef *ports[256]; // last byte is count
+
 
 int InitializeMCUSettings() {
 	//target_pinout pins;
@@ -10,19 +12,20 @@ int InitializeMCUSettings() {
 	pins[0]=GPIO_PIN_0;
 	pins[1]=GPIO_PIN_1;
 	pins[2]=GPIO_PIN_2;
-	pins[3]=GPIO_PIN_3;//gyro_cs
-	pins[4]=GPIO_PIN_4;//gyro exti
-	pins[5]=GPIO_PIN_5;//sbus inverter
-	pins[6]=GPIO_PIN_6;//vbus sense
-	pins[7]=GPIO_PIN_7;//usart 1 rx
-	pins[8]=GPIO_PIN_8;//usart 1 tx
-	pins[9]=GPIO_PIN_9;//usart 3 rx
-	pins[10]=GPIO_PIN_10;//usart 3 tx
-	pins[11]=GPIO_PIN_11;//usart 6 rx
-	pins[12]=GPIO_PIN_12;//usart 6 tx
-	pins[13]=GPIO_PIN_13;//rfbl bind1
-	pins[14]=GPIO_PIN_14;//rfbl bind2
+	pins[3]=GPIO_PIN_3;
+	pins[4]=GPIO_PIN_4;
+	pins[5]=GPIO_PIN_5;
+	pins[6]=GPIO_PIN_6;
+	pins[7]=GPIO_PIN_7;
+	pins[8]=GPIO_PIN_8;
+	pins[9]=GPIO_PIN_9;
+	pins[10]=GPIO_PIN_10;
+	pins[11]=GPIO_PIN_11;
+	pins[12]=GPIO_PIN_12;
+	pins[13]=GPIO_PIN_13;
+	pins[14]=GPIO_PIN_14;
 	pins[15]=GPIO_PIN_15;
+
 	pins[255]=16;
 
 
