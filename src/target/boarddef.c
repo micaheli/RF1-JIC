f73b0f0d37534aae5232d39bbcdc1cbc8766f6f5
Kalyn Doerr <kalyn.doerr@gmail.com>
2016-09-27 16:00:52 -0700

    serial

--- src/target/boarddef.c
+++ src/target/boarddef.c
@@ -1,6 +1,7 @@
 #include "includes.h"
 
 GPIO_TypeDef *ports[10];
+USART_TypeDef *usarts[6];
 
 
 int InitializeMCUSettings() {
@@ -18,7 +19,14 @@ int InitializeMCUSettings() {
 	ports[8]=GPIOJ;
 	ports[9]=GPIOK;
 
+	bzero(usarts, sizeof(usarts));
+	usarts[0]=USART1;
+	usarts[1]=USART2;
+	usarts[2]=USART3;
+	usarts[3]=USART4;
+	usarts[4]=USART5;
+	usarts[5]=USART6;
+
 
 	return(1);
-
 }
