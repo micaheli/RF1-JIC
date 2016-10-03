6578ba0e762840796b4b947d929db3f2d4ca965d
Kalyn Doerr <kalyn.doerr@gmail.com>
2016-10-03 10:28:25 -0700

    actuator initial

--- src/flight_controller/inc/drivers/actuator_output.h
+++ src/flight_controller/inc/drivers/actuator_output.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void OutputActuators(float motorOutput[], float servoOutput[]);
+void InitActuators(void);
--- src/flight_controller/src/drivers/actuator_output.c
+++ src/flight_controller/src/drivers/actuator_output.c
@@ -1,5 +1,13 @@
 #include "includes.h"
 
+void InitActuators(void) {
+
+}
+
+void InitActuatorTimer(void) {
+
+}
+
 void OutputActuators(float motorOutput[], float servoOutput[]) {
 	(void)(motorOutput);
 	(void)(servoOutput);
