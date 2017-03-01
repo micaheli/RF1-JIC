b9738057ab68a4658b813850a8335c89566f3769
Kalyn Doerr <kalyn.doerr@gmail.com>
2017-02-28 20:31:06 -0700

    cleanup

--- src/new_bootloader/src/rfbl.c
+++ src/new_bootloader/src/rfbl.c
@@ -22,7 +22,7 @@ uint32_t checkTooManyStarts = 0;
 uint32_t bootupDelay        = 155;
 uint32_t upgradeSkip        = ADDRESS_RFBL_START;
 uint32_t isRfbl             = 1;
-uint32_t rfblTagToUse       = RFBL_TAG;
+char rfblTagString[20]      = RFBL_TAG; //used to store a string in the flash. :)
 #else //Not RFBL so it must be recovery which is bootloader 1
 uint32_t bootloaderAddress  = ADDRESS_RECOVERY_START;
 uint32_t applicationAddress = ADDRESS_RFBL_START;
@@ -33,10 +33,10 @@ uint32_t checkTooManyStarts = 1;
 uint32_t bootupDelay        = 0;
 uint32_t upgradeSkip        = 0;
 uint32_t isRfbl             = 0;
-uint32_t rfblTagToUse       = RCVR_TAG;
+char rfblTagString[20]      = RCVR_TAG; //used to store a string in the flash. :)
 #endif
 
-char rfblTagString[20] = rfblTagToUse; //used to store a string in the flash. :)
+
 
 
 static void HandleBackupRegistersAndBooting(void);
