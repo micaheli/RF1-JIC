c7de1976ab02948fe77650492c37b360d278b4b6
Preston Garrison <Preston Garrison>
2017-05-23 00:45:56 -0700

    windows changes

---
+++ scripts/openocd.bat
@@ -0,0 +1,2 @@
+"c:\Program Files\GNU ARM Eclipse\OpenOCD\0.10.0-201704182147-dev\bin\openocd.exe" -s "c:\Program Files\GNU ARM Eclipse\OpenOCD\0.10.0-201704182147-dev\scripts" -f "c:\Program Files\GNU ARM Eclipse\OpenOCD\0.10.0-201704182147-dev\scripts\interface\stlink-v2.cfg" -f "c:\Program Files\GNU ARM Eclipse\OpenOCD\0.10.0-201704182147-dev\scripts\target\stm32f4x.cfg"
+
