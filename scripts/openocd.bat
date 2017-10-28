dd90b6e66804c272c1221719297d8bac99ee3aac
Preston Garrison <preston@raceflight.net>
2017-10-28 15:32:31 -0700

    openocd fixes

--- scripts/openocd.bat
+++ scripts/openocd.bat
@@ -1,2 +1,2 @@
-"C:\dev\openocd-20160901\bin\openocd.exe" -s "C:\dev\openocd-20160901\share\openocd\scripts" -f "C:\dev\openocd-20160901\share\openocd\scripts\interface\stlink-v2.cfg" -f "C:\dev\openocd-20160901\share\openocd\scripts\target\stm32f4x.cfg"
+"C:\dev\openocd\bin\openocd.exe" -s "C:\dev\openocd\share\openocd\scripts" -f "C:\dev\openocd\share\openocd\scripts\interface\stlink-v2.cfg" -f "C:\dev\openocd\share\openocd\scripts\target\stm32f4x.cfg"
 
